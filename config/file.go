7d9a53f4af3b25e8234f4b8229e739e22f18f382
michaelzhao <michaelpublic@qq.com>
2020-08-18 16:16:56 +0800

    update config

--- config/file.go
+++ config/file.go
@@ -2,16 +2,20 @@ package config
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"path/filepath"
 
+	"github.com/flosch/pongo2"
 	"github.com/ghodss/yaml"
 )
 
 // ReadJSONFromFile read a JSON format file or a YAML format
 // file then convert to JSON
-func ReadJSONFromFile(fp string) (b []byte, err error) {
-	if b, err = ioutil.ReadFile(fp); err != nil {
+func ReadJSONFromFile(fp string, vals M) (b []byte, err error) {
+	tpl, err := pongo2.FromFile(fp)
+	if err != nil {
+		return
+	}
+	if b, err = tpl.ExecuteBytes(pongo2.Context(vals)); err != nil {
 		return
 	}
 	switch ext := filepath.Ext(fp); ext {
@@ -22,9 +26,37 @@ func ReadJSONFromFile(fp string) (b []byte, err error) {
 	}
 }
 
+func ReadYAMLFromFile(fp string, vals M) (b []byte, err error) {
+	tpl, err := pongo2.FromFile(fp)
+	if err != nil {
+		return
+	}
+	if b, err = tpl.ExecuteBytes(pongo2.Context(vals)); err != nil {
+		return
+	}
+	switch ext := filepath.Ext(fp); ext {
+	case ".json":
+		return yaml.JSONToYAML(b)
+	default:
+		return
+	}
+}
+
 // Wrap ReadJSONFromFile, unmarhsal JSON to object automatically
-func FromFile(fp string, cfg interface{}) (err error) {
-	b, err := ReadJSONFromFile(fp)
+func FromFile(fp string, vals M) (_ M, err error) {
+	b, err := ReadJSONFromFile(fp, vals)
+	if err != nil {
+		return
+	}
+	doc := M{}
+	if err = json.Unmarshal(b, &doc); err != nil {
+		return
+	}
+	return doc, nil
+}
+
+func UnmarshalFile(fp string, vals M, cfg interface{}) (err error) {
+	b, err := ReadJSONFromFile(fp, vals)
 	if err != nil {
 		return
 	}
---
+++ config/mongo.go
@@ -0,0 +1,59 @@
+package config
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/connstring"
+)
+
+type M map[string]interface{}
+
+func FromMongo(
+	ctx context.Context, dburi string, collections ...string) (
+	_ M, err error) {
+	// get database name from uri
+	var cs connstring.ConnString
+	if cs, err = connstring.ParseAndValidate(dburi); err != nil {
+		return
+	}
+	database := cs.Database
+	// connect mongodb
+	cli, err := mongo.NewClient(options.Client().ApplyURI(dburi))
+	if err != nil {
+		return
+	}
+	if err = cli.Connect(ctx); err != nil {
+		return
+	}
+	db := cli.Database(database)
+	// query collection names
+	fmt.Println(len(collections))
+	if len(collections) == 0 {
+		if collections, err = db.ListCollectionNames(ctx, bson.D{}); err != nil {
+			return
+		}
+	}
+	fmt.Println(len(collections))
+	// query documents
+	tables := M{}
+	for _, name := range collections {
+		var cur *mongo.Cursor
+		if cur, err = db.Collection(name).Find(ctx, bson.D{}); err != nil {
+			return
+		}
+		table := []M{}
+		for cur.Next(ctx) {
+			doc := M{}
+			if err = cur.Decode(&doc); err != nil {
+				return
+			}
+			table = append(table, doc)
+		}
+		tables[name] = table
+	}
+	return tables, nil
+}
