f9b27ef05f77737b5340072096cebd7473fab505
michaelzhao <michaelpublic@qq.com>
2021-10-29 15:26:39 +0800

    update redis client

--- redis/client.go
+++ redis/client.go
@@ -1,31 +1,12 @@
 package redis
 
 import (
-	"context"
-	"time"
-
 	"github.com/go-redis/redis/v8"
 )
 
 const Nil = redis.Nil
 
-type Cmd = redis.Cmd
-type IntCmd = redis.IntCmd
-type BoolCmd = redis.BoolCmd
-type StatusCmd = redis.StatusCmd
-type StringCmd = redis.StringCmd
-
-type Client interface {
-	Close() error
-	Ping(ctx context.Context) *StatusCmd
-	Get(context.Context, string) *StringCmd
-	Set(context.Context, string, interface{}, time.Duration) *StatusCmd
-	SetNX(context.Context, string, interface{}, time.Duration) *BoolCmd
-	Del(context.Context, ...string) *IntCmd
-	EvalSha(context.Context, string, []string, ...interface{}) *Cmd
-	ScriptLoad(context.Context, string) *StringCmd
-	ScriptFlush(context.Context) *StatusCmd
-}
+type Client = redis.Cmdable
 
 var _ Client = (*redis.Client)(nil)
 var _ Client = (*redis.ClusterClient)(nil)
